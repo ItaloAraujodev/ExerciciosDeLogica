81b96073888e05f0389f1d750714adb54a6989ae
ItaloAraujodev <italoarauju2019@gmail.com>
2024-02-19 11:14:12 -0300

    feat: Resolvendo problema "duplicateEncoder"

---
+++ duplicateEncoder.js
@@ -0,0 +1,12 @@
+function duplicateEncode(word){
+  let result = '';
+  word = word.toLowerCase();
+  for (let i = 0; i < word.length; i++) {
+    if (word.indexOf(word[i]) === word.lastIndexOf(word[i])) {
+      result += '(';
+    } else {
+      result += ')';
+    }
+  }
+  return result;
+}
